e72ad029a482d541fdbc6ad11dbd37ee19382a1f
codahq <codahq@gmail.com>
2022-08-03 01:26:24 -0600

    more wednesday

--- wednesday.js
+++ wednesday.js
@@ -5,6 +5,8 @@ var wednesday = {
     this.Message = message;
     this.MessageSize = messageSize;
     this.MessageOffset = 50;
+    this.CanPlay = true;
+    this.PlayChance = 2000;
     return this;
   },
 
@@ -21,11 +23,40 @@ var wednesday = {
       ctx.moveTo(duck.x - 3, duck.y + duck.bob + 3);
       ctx.lineTo(duck.x + (this.MessageOffset - 5), duck.y + duck.bob - (this.MessageOffset - 5));
       ctx.stroke();
+      if (duck.special != null && this.shouldPlay() && this.canPlay()) {
+        this.play(duck);
+      }
     }
   },
 
   measure: function(ctx, duck) {
     ctx.font = this.MessageSize + 'px monospace';
     return ctx.measureText(this.Message).width + this.MessageOffset;
+  },
+
+  shouldPlay() {
+    return Math.floor(Math.random() * this.PlayChance) % this.PlayChance == 0;
+  },
+
+  canPlay() {
+    if (this.CanPlay) {
+      this.CanPlay = false;
+      return true;
+    }
+    return false;
+  },
+
+  play(duck) {
+    var audio = new Audio('It Is Wednesday My Dudes.mp3');
+    var promise = audio.play();
+
+    if (promise !== undefined) {
+      promise.then(_ => {
+        // Autoplay started!
+      }).catch(error => {
+        //autoplay policy prevented playback... fallback to changing the message I guess.
+        this.Message = 'Quack!';
+      });
+    }
   },
 };
